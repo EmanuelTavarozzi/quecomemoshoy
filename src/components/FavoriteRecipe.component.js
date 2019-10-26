28f5833f57cc9e7456311659b6993f16694245f3
Emanuel <emanuelandrestavarozzi@gmail.com>
2019-10-26 20:11:55 -0300

    Recetas favoritas terminadas

--- src/components/FavoriteRecipe.component.js
+++ src/components/FavoriteRecipe.component.js
@@ -1,8 +1,9 @@
 import React from 'react'
 //import {Link} from "react-router-dom";
-import {Container,Row,Col} from 'reactstrap'
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { faTrophy, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Container, Row, Col} from 'react-bootstrap'
+
 
 
 export default class FavoriteRecipe extends React.Component{
@@ -14,20 +15,53 @@ export default class FavoriteRecipe extends React.Component{
     }
     render(){
         return(
-                <Container className="contenedorReceta" >
-                    <Row>
-                        <Col style={{color:this.props.color}}lg="4" ><FontAwesomeIcon style={{margin:"1.5rem"}}icon={faTrophy} size="10x"/></Col>
-                        <Col lg="4">
-                            <h2>Ensalada de atún</h2>
-                            <p>Lorem ipsum dolor sit amet vivamus sed mi et felis</p>
+                /*
+                <div className="contenedorReceta" width="80%">
+                    <div style={{flexBasis:"25%"}}>
+                        <FontAwesomeIcon style={{margin:"2rem auto",textAlign:"center",color:this.props.color}}icon={faTrophy} size="8x"/>
+                    </div>
+                    <div style={{flexBasis:"50%"}}>
+                        <h3>Ensalada de atún</h3>
+                        <p>Lorem ipsum dolor sit amet vivamus sed mi et felis</p>
+                    </div>
+                    <div style={{flexBasis:"25%"}}>
+                        <div className="contenedorLikes">
+                            <FontAwesomeIcon icon={faThumbsUp} size="2x"/>
+                            <h3 style={{textAlign:"center",marginLeft:"1rem"}}>{this.props.likes}</h3>
+                        </div>
+                        <img style={{borderRadius:"1.5rem",maxWidth:"100%"}} alt="No hay imagen"src={require('../img/banner.jpg')}></img>
+                    </div>
+                </div>
+                */
+               <Container className="contenedorReceta" width="80%">
+                   <Row xs="12" lg="12" styles={{alignItems:"center"}}>
+                        <Col lg="6" xs="12">
+                            <h2 className="title">Ensalada con atún</h2>
                         </Col>
-                       <Col lg="4" >
-                        <h3 style={{textAlign:"end"}}>{this.props.likes}</h3>
-                        <img style={{borderRadius:"1.5rem"}}width="100%" alt="No hay imagen"src={require('../img/banner.jpg')}></img>
-                       </Col>
-                        
+                        <Col lg="6" xs="12">
+                            <div className="contenedorIconos">
+                                <div>
+                                    <FontAwesomeIcon style={{color:"#3EC5BD"}} className="iconos"icon={faThumbsUp} size="2x"/>
+                                    <h3 className="iconos" style={{marginLeft:"1rem"}}>{this.props.likes}</h3>
+                                </div>
+                                <FontAwesomeIcon className="iconos"style={{color:this.props.color}}icon={faTrophy} size="2x"/> 
+                            </div>  
+                        </Col>
+                   </Row>
+                    <Row xs="12" lg="12" style={{alignItems:"center"}}>
+                            <Col styles={{textAlign:"right"}}>
+                                < p > Lorem ipsum dolor sit amet, consectetur adipiscing elit.Cras efficitur gravida turpis, id egestas sapien convallis in .Suspendisse vitae egestas mi.Vivamus tincidunt, leo nec consequat auctor, metus elit dictum massa, sit amet bibendum dui est nec tortor.Praesent dapibus vulputate tristique.Pellentesque tempus erat eget tempus facilisis.Etiam volutpat non lectus nec egestas.Aliquam eget augue ac nisl luctus venenatis.Aliquam et lectus a arcu tempus laoreet ut ut ex.Sed rhoncus dolor sed scelerisque vestibulum.Integer sed porttitor neque.Nulla vel arcu non elit porttitor hendrerit a sit amet diam.Duis non augue sed velit varius sodales. </p>
+                            </Col>
+                            <Col>
+                                <img style={{borderRadius:"1.5rem",maxWidth:"100%"}} alt="No hay imagen"src={require('../img/banner.jpg')}></img>
+                            </Col>
                     </Row>
-                </Container>
+
+               </Container>
+               
+
+               
+            
         )
     }
 }

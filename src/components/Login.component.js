4c68bac79fcf430973da1d054192630ed7792d58
Emanuel <emanuelandrestavarozzi@gmail.com>
2019-11-19 21:23:14 -0300

    Empezando form login

--- src/components/Login.component.js
+++ src/components/Login.component.js
@@ -3,13 +3,59 @@ import React, {Component} from 'react'
 import {Container, Row, Col} from 'reactstrap'
 
 export default class Landing extends Component{
+    constructor(){
+        super()
+        this.state = {
+            isLogin:true,
+            email: "",
+            password:"",
+            active: "btn-form",
+            disabled:"btn-form-disabled"
+        }
+        this.handlechange = this.handlechange.bind(this)
+    }
 
+
+    handlechange(){
+        this.setState((prevState) => {
+            return {
+                isLogin:!prevState.isLogin,
+                active: prevState.disabled,
+                disabled: prevState.active
+            }
+        })
+    }
     render(){
         return(
-                <Container fluid= "true">
-                    <Row><Col>
-                    </Col></Row>
-                    <p>Welcome to Login</p>
+                <Container className="contenedorLogin">
+                    <div><h2 className="title" style={{fontSize:"6rem"}}>Que comemos hoy</h2></div>
+                    <div><h1 style={{letterSpacing:"1.2rem"}}>Aprender comiendo</h1></div>
+                    <div className="contenedorForm" fluid={true}>
+                        <Row>
+                            <Col lg="6" xs="12">
+                                <button className={this.state.active} onClick={this.handlechange} name="login">Log In</button>
+                            </Col>
+                            <Col lg="6" xs="12">
+                                <button className={this.state.disabled} name="sing-up" onClick={this.handlechange}>Sign Up</button>
+                            </Col>
+                        </Row>
+                        {this.state.isLogin ? 
+                        <Row>
+                            <form>
+                                <input name="email" placeholder="Correo electrónico"></input>
+                            </form>
+                        </Row>
+                        
+                        
+                        
+                        : 
+                        <Row>
+                            <h1>Honasndjo</h1>
+                        </Row>
+                        
+                        
+                        }
+                    </div>
                 </Container>
         )
     }

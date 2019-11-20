007471b7f31722e329d39b9b2845cb2134ecc9b8
Emanuel <emanuelandrestavarozzi@gmail.com>
2019-11-19 23:29:18 -0300

    Falta acomodar los input y ya queda listo

--- src/components/Login.component.js
+++ src/components/Login.component.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 //import {Link} from "react-router-dom";
 import {Container, Row, Col} from 'reactstrap'
-
+import LoadingPage from './Loading.component'
 export default class Landing extends Component{
     constructor(){
         super()
         this.state = {
+            isLoading:true,
             isLogin:true,
             email: "",
             password:"",
@@ -14,7 +15,12 @@ export default class Landing extends Component{
         }
         this.handlechange = this.handlechange.bind(this)
     }
-
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({isLoading:false})
+        },1000)
+    }
+    
 
     handlechange(){
         this.setState((prevState) => {
@@ -27,10 +33,14 @@ export default class Landing extends Component{
     }
     render(){
         return(
+            this.state.isLoading ?
+            <LoadingPage />
+            :
+            <div>
                 <Container className="contenedorLogin">
-                    <div><h2 className="title" style={{fontSize:"6rem"}}>Que comemos hoy</h2></div>
-                    <div><h1 style={{letterSpacing:"1.2rem"}}>Aprender comiendo</h1></div>
-                    <div className="contenedorForm" fluid={true}>
+                    <div data-aos="fade-right"><h2 className="title" style={{fontSize:"6rem"}}>Que comemos hoy</h2></div>
+                    <div data-aos="fade-right"><h1 style={{letterSpacing:"1.2rem"}}>Aprender comiendo</h1></div>
+                    <div className="contenedorForm" fluid={true} data-aos="fade-left">
                         <Row>
                             <Col lg="6" xs="12">
                                 <button className={this.state.active} onClick={this.handlechange} name="login">Log In</button>
@@ -40,23 +50,38 @@ export default class Landing extends Component{
                             </Col>
                         </Row>
                         {this.state.isLogin ? 
-                        <Row>
-                            <form>
-                                <input name="email" placeholder="Correo electrónico"></input>
-                            </form>
-                        </Row>
+                        <div className="animated fadeInRight">
+                            <Row className="login-form">
+                                <Col>
+                                    <form style={{display:"flex",flexDirection:"column"}}>
+                                        <label><input name="email" placeholder="Correo electrónico"></input></label>
+                                        <label><input name="password" placeholder="Contraseña"></input></label>
+                                        <button className="btn-log"> Log In</button>
+                                    </form>
+                                </Col>
+                            </Row>
+                        </div>
                         
                         
                         
-                        : 
-                        <Row>
-                            <h1>Honasndjo</h1>
-                        </Row>
-                        
+                        :
+                        <div className="animated fadeInLeft"> 
+                            <Row className="login-form">
+                                <Col>
+                                    <form style={{display:"flex",flexDirection:"column"}}>
+                                        <label><input name="email" placeholder="Correo electrónico"></input></label>
+                                        <label><input name="password" placeholder="Contraseña"></input></label>
+                                        <button className="btn-log"> Sign Up </button>  
+                                    </form> 
+                                </Col>
+                            </Row>
+                        </div>
                         
                         }
                     </div>
+                
                 </Container>
+         </div>       
         )
     }
 
